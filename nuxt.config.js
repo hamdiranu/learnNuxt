d36de0637bbc8c2126a29fb8a36afa07702d944b
hamdiranu <hamdira@sci.ui.ac.id>
2021-08-05 16:49:50 +0700

    fix babel config

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,5 +50,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]],
+    },
   }
 }
